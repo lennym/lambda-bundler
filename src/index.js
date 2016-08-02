ffd1c932924e312c3a1818e37e9ef33e68315333
Leonard Martin <leonard.martin@gmail.com>
2016-08-02 16:32:36 +0100

    Add middleware stack
    Executes functions in turn on lambda invocation, allowing each to modify the event and context as they pass through.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,29 @@
+'use strict';
 const lambda = require('./');
+const pkg = require('./package.json')['lambda-bundler'] || {};
+
+const middleware = pkg.middleware || [];
+middleware.push(lambda);
+
+const stack = middleware.reduce((fn, key) => {
+  let layer = key;
+  if (typeof key === 'string') {
+    layer = require(key);
+  }
+  if (typeof layer !== 'function' || layer.length !== 3) {
+    throw new Error(`Invalid middleware: ${key}`);
+  }
+  return (event, context, callback) => {
+    fn(event, context, (err, data) => {
+      if (err) {
+        callback(err);
+      } else {
+        layer(event, context, callback);
+      }
+    });
+  };
+}, (e, c, callback) => { callback(); });
 
 module.exports = {
-  handler: function (event, context, callback) {
-    lambda(event, context, callback);
-  }
+  handler: stack
 };
