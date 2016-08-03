716dd9460d4d4fe0316247cc853223920e195994
Leonard Martin <leonard.martin@gmail.com>
2016-08-03 14:41:48 +0100

    Improve middleware stacking
    Allow erros thrown at any one middleware layer to be passed back through previous layers' outbound callbacks.

--- src/index.js
+++ src/index.js
@@ -25,44 +25,31 @@ const stack = middleware.map((key) => {
   return { inbound, outbound };
 });
 
-// create a recursive function which iterates through our middleware stack
-const inbound = stack.reduce((fn, layer) => {
-  return (event, context, done) => {
-    fn(event, context, (err) => {
-      if (err) {
-        done(err);
+// returns a function wrapped inside the inbound and outbound callbacks of a middleware layer
+function wrap (inner, layer) {
+  return (event, context, callback) => {
+    layer.inbound(event, context, (inerr) => {
+      if (inerr) {
+        callback(inerr);
       } else {
-        layer.inbound(event, context, done);
-      }
-    });
-  };
-}, (e, c, done) => done());
-
-// and a second recursive function to iterate back through outbound middleware when complete
-const outbound = stack.reduceRight((fn, layer) => {
-  return (err, response, done) => {
-    fn(err, response, (e) => {
-      layer.outbound(e, response, done);
-    });
-  };
-}, (err, response, done) => done(err));
-
-module.exports = {
-  handler: (event, context, callback) => {
-    inbound(event, context, (err) => {
-      if (err) {
-        callback(err);
-      } else {
-        lambda(event, context, (err, response) => {
-          outbound(err, response, (err) => {
-            if (err) {
-              callback(err);
+        inner(event, context, (err, result) => {
+          layer.outbound(err, result, (outerr) => {
+            if (outerr) {
+              callback(outerr);
             } else {
-              callback(null, response);
+              callback(null, result);
             }
           });
         });
       }
     });
-  }
+  };
+}
+
+function nest (inner) {
+  return stack.reduceRight(wrap, inner);
+}
+
+module.exports = {
+  handler: nest(lambda)
 };
